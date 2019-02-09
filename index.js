37e68f397cedd744bc28d8075924b594a782b440
Jesse Atay <jesse.atay@gmail.com>
2019-02-08 21:14:08 -0800

    arithmetic tests pass

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 'use strict'
 
+//MODULE IMPORTS    
 const greetImport = require('../lab01/greet');
+const mathImport = require('../lab01/arithmetic');
+const http = require('http');
 
-console.log(greetImport.requiredGreeting('Jesse'));
+//MODULE METHODS STORED IN VARIABLES
+const greetingTest = greetImport.requiredGreeting('Jesse');
+const mathTest = mathImport.testAdd(1, 2);
+
+
+//SERVER FOR TEST RUN  
+const requestHandler = (req, res) => {
+    res.setHeadder('Content-Type', 'text/html');
+    res.statusCode = 200;
+    res.write(greetingTest);
+    res.write(mathTest);
+    res.end();
+}
+
+
+//SERVER RUN
+const app = http.createServer(requestHandler);
+app.listen(process.env.PORT, () => console.log('server is listening'));
+
+
+//MODULE METHOD CONSOLE LOGS    
+console.log(greetingTest);
+console.log(mathTest);
